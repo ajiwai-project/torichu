94333e4e5541782b8fdcc32b2b7416a8dbd72804
Takashi Makino <t-makino@atware.co.jp>
2023-07-15 00:09:31 +0900

    #1 Create point enum

--- lib/features/models/cost.dart
+++ lib/features/models/cost.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_template/features/models/point.dart';
+
 class Cost {
   static const _maxTitleLength = 100;
   static const _maxAmount = 9999999;
-  static const _pointPattern = {1, 2, 3, 5, 8, 13, 21, 34, 55};
   static const _categoryPattern = {
     '食費',
     '趣味・娯楽',
@@ -16,13 +17,13 @@ class Cost {
 
   late String _title;
   late int _amount;
-  late int _point;
+  late Point _point;
   late String _category;
 
   Cost(
       {required String title,
       required int amount,
-      required int point,
+      required Point point,
       required String category}) {
     if (title.length > _maxTitleLength) {
       throw Exception('Title must be 100 characters or less.');
@@ -30,10 +31,6 @@ class Cost {
 
     if (amount > _maxAmount) {
       throw Exception('Amount must be less than 9999999');
-    }
-
-    if (!_pointPattern.contains(point)) {
-      throw Exception('Point must be Fibonacci number below 55');
     }
 
     if (!_categoryPattern.contains(category)) {
@@ -48,6 +45,6 @@ class Cost {
 
   String get title => _title;
   int get amount => _amount;
-  int get point => _point;
+  Point get point => _point;
   String get category => _category;
 }
--- test/features/models/cost_test.dart
+++ test/features/models/cost_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_template/features/models/cost.dart';
+import 'package:flutter_template/features/models/point.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const dummyTitle = 'dummy';
   const dummyAmount = 1;
-  const dummyPoint = 1;
+  const dummyPoint = Point.one;
   const dummyCategory = '食費';
 
   String generateStrings(int num) {
@@ -15,7 +16,7 @@ void main() {
     test('should have the values', () {
       var title = 'title';
       var amount = 10;
-      var point = 1;
+      var point = Point.two;
       var category = '食費';
       var cost =
           Cost(title: title, amount: amount, point: point, category: category);
@@ -48,65 +49,6 @@ void main() {
               category: dummyCategory),
           throwsException);
     });
-
-    final nonFibonacciBolow55 = [
-      4,
-      6,
-      7,
-      9,
-      11,
-      12,
-      14,
-      15,
-      16,
-      17,
-      18,
-      19,
-      20,
-      22,
-      23,
-      24,
-      25,
-      16,
-      27,
-      28,
-      29,
-      30,
-      31,
-      32,
-      33,
-      36,
-      37,
-      38,
-      39,
-      40,
-      41,
-      42,
-      43,
-      44,
-      45,
-      46,
-      47,
-      48,
-      49,
-      50,
-      51,
-      52,
-      53,
-      54
-    ];
-
-    for (final point in nonFibonacciBolow55) {
-      test('should throw when point is not Fibonacci number below 55', () {
-        expect(
-            () => Cost(
-                title: dummyTitle,
-                amount: dummyAmount,
-                point: point,
-                category: dummyCategory),
-            throwsException);
-      });
-    }
 
     test('should throw when category is invalid', () {
       expect(
