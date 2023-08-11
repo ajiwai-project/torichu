4550f0351133c421248a3fcd56dc2d19b49d817c
Takashi Makino <t-makino@atware.co.jp>
2023-08-11 11:08:33 +0900

    #29 Add tag model

---
+++ lib/domain/cost/tag.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'tag.freezed.dart';
+
+@freezed
+class Tag with _$Tag {
+
+  @Assert('value.isNotEmpty')
+  @Assert('value.length <= 30')
+  factory Tag._({required String value}) = _Tag;
+
+  factory Tag.of(String value) {
+    return Tag._(value: value);
+  }
+}
---
+++ test/domain/cost/tag_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_template/domain/cost/tag.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Factory', () {
+    test('should have specified value', () {
+      final tag = Tag.of('tag');
+      expect(tag.value, 'tag');
+    });
+  });
+
+  test('should throw when tag value is emtpy', () {
+    expect(() => Tag.of(''), throwsA(isA<AssertionError>()));
+  });
+
+  test('should throw an exception when tag value is too long', () {
+    expect(() => Tag.of('a' * 31), throwsA(isA<AssertionError>()));
+  });
+}
