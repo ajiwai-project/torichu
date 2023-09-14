8ebff563e100aa16a71134df0c3318cc7da32f33
okada takuma <takuma@pop-os.localdomain>
2023-09-14 17:07:46 +0900

    単体テストを作成

---
+++ test/presentation/widgets/tabbar/tabbar.test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/presentation/features/cost_list_viewer/cost_list_viewer_page.dart';
+import 'package:flutter_template/presentation/features/registration/registration_page.dart';
+import 'package:flutter_template/presentation/widgets/tabbar/tabbar.dart';
+
+void main() {
+  render(tester) async {
+    await tester.pumpWidget(ProviderScope(overrides: [
+      costRepositoryProvider.overrideWith((ref) => mockCostRepository)
+    ], child: const MaterialApp(home: BottomTabPage())));
+  }
+
+  testWidgets('should registeration page when opening app', (tester) async {
+    await render(tester);
+    expect(find.byType(RegisterationPage), findsOneWidget);
+  });
+
+  testWidgets('should move page when tap tab', (tester) async {
+    await render(tester);
+    await tester.tap(find.byKey(const Key('costs_tab')));
+    await tester.pumpAndSettle();
+    expect(find.byType(CostListViewerPage), findsOneWidget);
+    await tester.tap(find.byKey(const Key('registration_tab')));
+    await tester.pumpAndSettle();
+    expect(find.byType(RegisterationPage), findsOneWidget);
+  });
+}
