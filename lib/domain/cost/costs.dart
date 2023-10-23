016f7bb37de2956d7c96e7bf561b0d4eeb3efd5e
okada takuma <takuma@pop-os.localdomain>
2023-10-23 14:55:47 +0900

    参照されない関数を削除

--- lib/domain/cost/costs.dart
+++ lib/domain/cost/costs.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:flutter_template/domain/cost/category.dart';
 import 'package:flutter_template/domain/cost/cost.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -11,39 +10,6 @@ class Costs with _$Costs {
 }
 
 extension CostsExtention on Costs {
-  int get sumOfAmount =>
-      values.map((e) => e.amount.value).fold(0, (a, b) => a + b);
-  int get sumOfPoint =>
-      values.map((e) => e.point.value).fold(0, (a, b) => a + b);
-
-  int get length => values.length;
-
-  Map<Category, int> get amountGroupByCategory {
-    Map<Category, int> amountMap = {};
-
-    groupBy<Cost, Category>(values, (value) => value.category)
-        .entries
-        .forEach((entry) {
-      amountMap[entry.key] =
-          entry.value.map((e) => e.amount.value).fold(0, (a, b) => a + b);
-    });
-
-    return amountMap;
-  }
-
-  Map<Category, int> get pointGroupByCategory {
-    Map<Category, int> pointMap = {};
-
-    groupBy<Cost, Category>(values, (value) => value.category)
-        .entries
-        .forEach((entry) {
-      pointMap[entry.key] =
-          entry.value.map((e) => e.point.value).fold(0, (a, b) => a + b);
-    });
-
-    return pointMap;
-  }
-
   Cost get(int index) => values[index];
 
   Costs sortByRegisteredAt({bool desc = true}) {
--- test/domain/cost/costs_test.dart
+++ test/domain/cost/costs_test.dart
@@ -6,60 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'model_support.dart';
 
 void main() {
-  group('AmountGroupByCategory', () {
-    test('should return sum of amount for each category', () {
-      var cost1 =
-          CostBuilder().setAmount(500).setCategory(Category.food).build();
-      var cost2 =
-          CostBuilder().setAmount(300).setCategory(Category.food).build();
-      var cost3 =
-          CostBuilder().setAmount(300).setCategory(Category.hobbies).build();
-      var cost4 = CostBuilder()
-          .setAmount(700)
-          .setCategory(Category.entertainment)
-          .build();
-      var costs = Costs(values: [cost1, cost2, cost3, cost4]);
-
-      var actual = costs.amountGroupByCategory;
-
-      var expected = {
-        Category.food: 800,
-        Category.hobbies: 300,
-        Category.entertainment: 700
-      };
-
-      expect(actual, expected);
-    });
-  });
-
-  group('PointGroupByCategory', () {
-    test('should return sum of point for each category', () {
-      var cost1 =
-          CostBuilder().setPoint(Point.two).setCategory(Category.food).build();
-      var cost2 =
-          CostBuilder().setPoint(Point.five).setCategory(Category.food).build();
-      var cost3 = CostBuilder()
-          .setPoint(Point.one)
-          .setCategory(Category.hobbies)
-          .build();
-      var cost4 = CostBuilder()
-          .setPoint(Point.thirteen)
-          .setCategory(Category.entertainment)
-          .build();
-      var costs = Costs(values: [cost1, cost2, cost3, cost4]);
-
-      var actual = costs.pointGroupByCategory;
-
-      var expected = {
-        Category.food: 2 + 5,
-        Category.hobbies: 1,
-        Category.entertainment: 13
-      };
-
-      expect(actual, expected);
-    });
-  });
-
   group('SortByRegisteredAt', () {
     test('should sort in descending order when desc is true', () {
       var cost1 =
