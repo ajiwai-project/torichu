9af8be30a65a9272ea93982e9e380379d4da510c
okada takuma <takuma@pop-os.localdomain>
2023-11-19 21:26:34 +0900

    不要なファイルを削除

--- lib/presentation/features/routing/bottom_navigation_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'bottom_navigation_state.freezed.dart';
-
-@freezed
-class BottomNavigationState with _$BottomNavigationState {
-  const factory BottomNavigationState({@Default(0) int index}) =
-      _BottomNavigationState;
-}
