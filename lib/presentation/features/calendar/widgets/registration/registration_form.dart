685ef4e114e7f2a0d5b6e0fea3b533bccbffb53a
okada takuma <takuma@pop-os.localdomain>
2023-11-19 21:35:32 +0900

    input fieldのスタイルを変更

--- lib/presentation/features/calendar/widgets/registration/registration_form.dart
+++ lib/presentation/features/calendar/widgets/registration/registration_form.dart
@@ -16,13 +16,15 @@ class RegistrationForm extends HookConsumerWidget {
 
     return Scaffold(
         body: Padding(
-            padding: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(32),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(children: [
                   _TitleField((value) => viewModel.setTitle(value)),
+                  const SizedBox(height: 16),
                   _PriceField((value) => viewModel.setPrice(value)),
+                  const SizedBox(height: 16),
                   _SizeSelector(
                       state.size, (value) => viewModel.setSize(value)),
                 ]),
@@ -44,7 +46,9 @@ class _TitleField extends StatelessWidget {
         key: const Key('title-field'),
         decoration:
 
-            const InputDecoration(hintText: 'タイトルを入力', labelText: 'タイトル'),
+            const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'タイトルを入力', labelText: 'タイトル'),
         onChanged: onChanged);
   }
 }
@@ -58,7 +62,9 @@ class _PriceField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
         key: const Key('price-field'),
-        decoration: const InputDecoration(hintText: '金額を入力', labelText: '金額'),
+        decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '金額を入力', labelText: '金額'),
         keyboardType: TextInputType.number,
         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
         onChanged: (value) => onChanged(int.parse(value)));
@@ -84,7 +90,9 @@ class _SizeSelector extends StatelessWidget {
                     color: Theme.of(context).colorScheme.onBackground)));
       }).toList(),
       onChanged: (Size? value) => onChanged(value!),
-      decoration: const InputDecoration(hintText: 'サイズを選択', labelText: 'サイズ'),
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'サイズを選択', labelText: 'サイズ'),
     );
   }
 }
@@ -96,10 +104,10 @@ class _RegistrationButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton.icon(
+    return OutlinedButton.icon(
         key: const Key('register-button'),
         onPressed: () => onPressed(),
-        style: ElevatedButton.styleFrom(
+        style: OutlinedButton.styleFrom(
           padding: const EdgeInsets.all(16),
           foregroundColor: Theme.of(context).colorScheme.onBackground,
         ),
