5a87c30084fc38bafc82f999b74a531c80c83874
okada takuma <takuma@pop-os.localdomain>
2023-12-01 14:43:38 +0900

    iosの数値キーボードにDoneボタンを 表示する

--- lib/presentation/features/calendar/widgets/registration/registration_form.dart
+++ lib/presentation/features/calendar/widgets/registration/registration_form.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_template/domain/cost/size.dart';
@@ -9,28 +11,26 @@ class RegistrationForm extends HookConsumerWidget {
   final Function? onSuccess;
   const RegistrationForm({super.key, this.onSuccess});
 
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(registrationViewModelProvider);
     final viewModel = ref.watch(registrationViewModelProvider.notifier);
-    
+
     void handleOnSuccess() {
       if (onSuccess != null) {
         onSuccess!();
       }
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-          content: Text('Added to list'),
-          duration: Duration(seconds: 2),
-          margin: EdgeInsets.only(left: 23, right: 23, bottom: 23),
-          behavior: SnackBarBehavior.floating,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10)
-          ))
-        ),
+            content: Text('Added to list'),
+            duration: Duration(seconds: 2),
+            margin: EdgeInsets.only(left: 23, right: 23, bottom: 23),
+            behavior: SnackBarBehavior.floating,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)))),
       );
     }
+
     return Scaffold(
         body: Padding(
             padding: const EdgeInsets.all(32),
@@ -45,9 +45,8 @@ class RegistrationForm extends HookConsumerWidget {
                   _SizeSelector(
                       state.size, (value) => viewModel.setSize(value)),
                 ]),
-                _RegistrationButton(() => viewModel
-                    .register()
-                    .then((_) => handleOnSuccess())),
+                _RegistrationButton(
+                    () => viewModel.register().then((_) => handleOnSuccess())),
               ],
             )));
   }
@@ -81,7 +80,9 @@ class _PriceField extends StatelessWidget {
         key: const Key('price-field'),
         decoration: const InputDecoration(
             border: OutlineInputBorder(), hintText: '金額を入力', labelText: '金額'),
-        keyboardType: TextInputType.number,
+        keyboardType: Platform.isIOS
+            ? const TextInputType.numberWithOptions(signed: true)
+            : TextInputType.number,
         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
         onChanged: (value) => onChanged(int.parse(value)));
   }
